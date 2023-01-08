47a1001a4b48d0ab1d347a391da084f6ef539506
Oluwa-Seun <oluwaseunolubiyione@gmail.com>
2023-01-08 12:42:30 +0100

    styled component

--- src/section/About.js
+++ src/section/About.js
@@ -1,6 +1,117 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+
+import img1 from '../assets/Images/img1.jpeg';
+import lazyboy36 from '../assets/Images/lazyboy36.png';
+import lazyboy38 from '../assets/Images/lazyboy38.png';
+
+const Section = styled.section`
+position: relative;
+min-height: 100vh;
+width: 80vw;
+display: flex;
+margin: 0 auto;
+@media (max-width: 48em) {
+    width: 90vw;
+  }
+  @media (max-width: 30em) {
+    width: 100vw;
+  }
+`
+//delete overflow incase of bug 
+
+const Title = styled.h1`
+font-size: ${props => props.theme.fontBig};
+font-family: 'Panchang', sans-serif;
+font-wight: 300;
+position: absolute;
+top: 1rem;
+left: 6%;
+z-index:5;
+@media (max-width: 64em) {
+    font-size: ${(props) => `clac(${props.theme.fontBig} - 5vw)`};
+    top: 0;
+    left: 0;
+  }
+  @media (max-width: 48em) {
+    font-size: ${(props) => props.theme.fontxxxl};
+  }
+`
+
+const Left = styled.div`
+width: 50%;
+font-size: ${props => props.theme.fontlg};
+font-weight: 300;
+position: realtive;
+z-index: 5;
+margin-top: 20%;
+@media (max-width: 64em) {
+    width: 80%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) !important;
+    margin: 0 auto;
+    padding: 2rem;
+    font-weight: 600;
+    backdrop-filter: blur(2px);
+    background-color: ${(props) => `rgba(${props.theme.textRgba},0.4)`};
+    border-radius: 20px;
+  }
+  @media (max-width: 48em) {
+    font-size: ${(props) => props.theme.fontmd};
+  }
+  @media (max-width: 30em) {
+    font-size: ${(props) => props.theme.fontsm};
+    width: 70%;
+  }
+`
+
+const Right = styled.div`
+width: 50%;
+position: relative;
+img {
+    width: 100%;
+    height: auto;
+}
+.small-img-1 {
+    width:40%;
+    position: absolute;
+    right: 95%;
+    bottom: 10%
+}
+.small-img-2 {
+    width:40%;
+    position: absolute;
+    right: 8%;
+    bottom: 30%;
+}
+@media (max-width: 64em) {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      object-fit: cover;
+      height: 100vh;
+    }
+    .small-img-1 {
+      width: 30%;
+      height: auto;
+      left: 5%;
+      bottom: 10%;
+    }
+    .small-img-2 {
+      width: 30%;
+      height: auto;
+      left: 60%;
+      bottom: 20%;
+    }
+  }
+`
+
 const About = () => {
     return (
         <Section id="fixed-target">
