26ca41f2c441b82d12c1a4f0170a8d4240b8ff80
Oluwa-Seun <oluwaseunolubiyione@gmail.com>
2023-01-08 12:51:12 +0100

    styled components

--- src/section/Banner.js
+++ src/section/Banner.js
@@ -1,7 +1,55 @@
 import React from "react";
 import styled from "styled-components";
 
+const Section = styled.section`
+  min-height: 100vh;
+  width: 80vw;
+  margin: 0 auto;
+  display: flex;
 
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  @media (max-width: 48em) {
+    width: 90vw;
+  }
+`;
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: 64em) {
+    justify-content: center;
+  }
+`;
+const BannerComponent = styled.h1`
+  font-size: ${(props) => props.theme.fontxxxl};
+  font-family: 'Panchang', sans-serif;
+  color: ${(props) => props.theme.text};
+  white-space: nowrap;
+  text-transform: uppercase;
+  line-height: 1;
+  span {
+    display: block;
+    background-color: ${(props) => props.theme.body};
+    padding: 1rem 2rem;
+  }
+  @media (max-width: 70em) {
+    font-size: ${(props) => props.theme.fontxxl};
+  }
+  @media (max-width: 64em) {
+    margin: 1rem 0;
+  }
+  @media (max-width: 48em) {
+    font-size: ${(props) => props.theme.fontlg};
+    margin: 0.5rem 0;
+  }
+  @media (max-width: 30em) {
+    font-size: ${(props) => props.theme.fontmd};
+  }
+`;
 const Banner = () => {
   return (
     <Section>
