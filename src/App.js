ddf5b47a7b6296587a8ca25d77eeea17ef3f5b34
Oluwa-Seun <oluwaseunolubiyione@gmail.com>
2023-01-11 15:52:19 +0100

    create app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import { useRef } from 'react';
 import 'locomotive-scroll/dist/locomotive-scroll.css';
 
 
+import Home from './section/Home';
+import About from './section/About';
+import Shop from './section/Shop';
+import Banner from './section/Banner';
+import Footer from './section/Footer';
+import NewArrival from "./section/NewArivals";
+
 
 import ScrollTriggerProxy from './components/scrollTriggerProxy';
 
@@ -14,7 +21,33 @@ function App() {
   const containerRef = useRef(null)
   return (
     <>
-      
+       <GlobalStyles />
+      <ThemeProvider theme={dark} >
+
+        <LocomotiveScrollProvider
+          options={
+            {
+              smooth: true,
+            }
+          }
+          watch={
+            [
+              ///
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <ScrollTriggerProxy />
+          <main className='App' data-scroll-container ref={containerRef}>
+            <Home />
+            <About />
+            <Shop />
+            <Banner />
+            <NewArrival />
+            <Footer />
+          </main>
+        </LocomotiveScrollProvider>
+      </ThemeProvider>
 
        
     </>
